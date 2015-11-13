f9a2f17252722eedbb8b34bd9032d5b2d08dfe30
briansalvattore <briancastillo008@gmail.com>
2015-11-13 00:21:59 -0500

    Update App.java

--- app/src/main/java/app/horses/console/red/App.java
+++ app/src/main/java/app/horses/console/red/App.java
@@ -9,11 +9,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        Parse.initialize(this,
-                "rn8gIUOfyOGB71PgGI29umb0rvRysKb4EvpyeNmC",
-                "zfMz77iHz5HGTfis0lg3g9rvM4gEAPuMxyARVlmA");
-
+        
         SpMain.init(this);
     }
 }
